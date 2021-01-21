1cb3304696d73a8af8fd7622d0c235d7292df0fe
foxching <rechielagria@gmail.com>
2021-01-21 19:08:05 +0800

    working on authentication routes/pre register

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose')
 const app = express();
 
 //body parser
@@ -11,6 +12,17 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/api/boards', require('./routes/api/boards'));
 app.use('/api/cards', require('./routes/api/cards'));
 app.use('/api/lists', require('./routes/api/lists'));
+app.use('/api/auth', require('./routes/api/auth'));
+
+
+
+//mongodb
+const config = require('config')
+const db = config.get('mongoURI')
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, })
+    .then(() => console.log('Database connected'))
+    .catch(error => console.log(error))
+
 
 
 //port
